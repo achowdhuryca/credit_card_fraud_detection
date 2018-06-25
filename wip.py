1abdfe795430d52152d9c1f060f34a992077083f
A Chowdhury <39204275+achowdhuryca@users.noreply.github.com>
2018-06-24 21:16:53 -0400

    python script

---
+++ wip.py
@@ -0,0 +1,308 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 21:13:21 2018
+
+@author: amin
+"""
+#Imports
+import sys
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import scipy
+
+#Versions
+print('Python: {}'.format(sys.version))
+print('Numpy: {}'.format(numpy.__version__))
+print('Pandas: {}'.format(pandas.__version__))
+print('Matplotlib: {}'.format(matplotlib.__version__))
+print('Seaborn: {}'.format(seaborn.__version__))
+print('Scipy: {}'.format(scipy.__version__))
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv('creditcard.csv')
+
+df.columns.values
+df.describe
+df.shape
+
+#Check Missing Data
+
+df.info()
+
+total = df.isnull().sum().sort_values(ascending = False)
+percent = (df.isnull().sum()/df.isnull().count()*100).sort_values(ascending = False)
+pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+# no miising values
+
+# Plot histograms of each parameter 
+df.hist(figsize = (20, 20))
+plt.show()
+
+
+#Data Unbalance
+temp = df["Class"].value_counts()
+df = pd.DataFrame({'Class': temp.index,'values': temp.values})
+plt.figure(figsize = (6,6))
+plt.title('Credit Card Fraud Class - data unbalance (Not fraud = 0, Fraud = 1)')
+sns.set_color_codes("pastel")
+sns.barplot(x = 'Class', y="values", data=df)
+locs, labels = plt.xticks()
+plt.show()
+
+#Only 492 (or 0.172%) of transaction are fraudulent. That means the data is highly unbalanced with respect with target variable Class.
+
+# Determine number of fraud cases in dataset
+
+Fraud = df[df['Class'] == 1]
+Valid = df[df['Class'] == 0]
+
+outlier_fraction = len(Fraud)/float(len(Valid))
+print(outlier_fraction)
+
+print('Fraud Cases: {}'.format(len(df[df['Class'] == 1])))
+print('Valid Transactions: {}'.format(len(df[df['Class'] == 0])))
+
+
+
+# Correlation matrix
+corrmat = df.corr()
+fig = plt.figure(figsize = (16, 12))
+
+sns.heatmap(corrmat, vmax = .8, square = True)
+plt.show()
+
+
+#Feature Correlation
+
+plt.figure(figsize = (14,14))
+plt.title('Credit Card Transactions features correlation plot (Pearson)')
+corr = df.corr()
+sns.heatmap(corr,xticklabels=corr.columns,yticklabels=corr.columns,linewidths=.1,cmap="Reds")
+plt.show()
+
+#As expected, there is no notable correlation between features V1-V28. There are certain correlations between some of these features and Time (inverse correlation with V3) and Amount (direct correlation with V7 and V20, inverse correlation with V1 and V5).
+
+# Features density plot
+
+var = df.columns.values
+
+i = 0
+t0 = df.loc[df['Class'] == 0]
+t1 = df.loc[df['Class'] == 1]
+
+sns.set_style('whitegrid')
+plt.figure()
+fig, ax = plt.subplots(8,4,figsize=(16,28))
+
+for feature in var:
+    i += 1
+    plt.subplot(8,4,i)
+    sns.kdeplot(t0[feature], bw=0.5,label="Class = 0")
+    sns.kdeplot(t1[feature], bw=0.5,label="Class = 1")
+    plt.xlabel(feature, fontsize=12)
+    locs, labels = plt.xticks()
+    plt.tick_params(axis='both', which='major', labelsize=12)
+plt.show();
+
+# =============================================================================
+# For some of the features we can observe a good selectivity in terms of distribution for the two values of Class: V4, V11 have clearly separated distributions for Class values 0 and 1, V12, V14, V18 are partially separated, V1, V2, V3, V10 have a quite distinct profile, whilst V25, V26, V28 have similar profiles for the two values of Class.
+# 
+# In general, with just few exceptions (Time and Amount), the features distribution for legitimate transactions (values of Class = 0) is centered around 0, sometime with a long queue at one of the extremities. In the same time, the fraudulent transactions (values of Class = 1) have a skewed (asymmetric) distribution.
+# =============================================================================
+
+#Predictive models - LightGBM
+
+params = {
+          'boosting_type': 'gbdt',
+          'objective': 'binary',
+          'metric':'auc',
+          'learning_rate': 0.05,
+          'num_leaves': 7,  # we should let it be smaller than 2^(max_depth)
+          'max_depth': 4,  # -1 means no limit
+          'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+          'max_bin': 100,  # Number of bucketed bin for feature values
+          'subsample': 0.9,  # Subsample ratio of the training instance.
+          'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+          'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
+          'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+          'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+          'nthread': 8,
+          'verbose': 0,
+          'scale_pos_weight':150, # because training data is extremely unbalanced 
+         }
+
+# Define predictors and target values
+
+target = 'Class'
+predictors = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',\
+       'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',\
+       'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',\
+       'Amount']
+
+
+# Split data in train and validation set
+
+#Let's define train and validation sets and then run a model using the train set for training and the validation set for validation. We output (every VERBOSE_EVAL step, set here to 50) both the training error and the validation error (calculated using the selected metric, AUC). The training will stop when we are not improving validation after a number of EARLY_STOP rounds. EARLY_STOP is selected to have value 50.
+
+#Libs
+
+
+from sklearn.model_selection import KFold
+
+import lightgbm as lgb
+import gc
+from datetime import datetime 
+from sklearn.model_selection import train_test_split
+
+
+
+#VALIDATION
+VALID_SIZE = 0.70 # simple validation using train_test_split
+
+#CROSS-VALIDATION
+NUMBER_KFOLDS = 5 #number of KFolds for cross-validation
+
+RANDOM_STATE = 2018
+
+MAX_ROUNDS = 1000 #lgb iterations
+EARLY_STOP = 50 #lgb early stop 
+OPT_ROUNDS = 1000  #To be adjusted based on best validation rounds
+VERBOSE_EVAL = 50 #Print out metric result
+
+
+
+train_df, val_df = train_test_split(df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
+
+dtrain = lgb.Dataset(train_df[predictors].values, 
+                     label=train_df[target].values,
+                     feature_name=predictors)
+del train_df
+gc.collect()
+
+dvalid = lgb.Dataset(val_df[predictors].values,
+                     label=val_df[target].values,
+                     feature_name=predictors)
+del val_df
+gc.collect()
+
+evals_results = {}
+
+model = lgb.train(params, 
+                  dtrain, 
+                  valid_sets=[dtrain, dvalid], 
+                  valid_names=['train','valid'], 
+                  evals_result=evals_results, 
+                  num_boost_round=MAX_ROUNDS,
+                  early_stopping_rounds=EARLY_STOP,
+                  verbose_eval=VERBOSE_EVAL, 
+                  feval=None)
+
+del dvalid
+
+# Best validation score was obtained for round 66, for which AUC = 0.978.
+
+
+# Training and validation using cross-validation
+#Let's use now cross-validation. We will use cross-validation (KFolds) with 5 folds. Data is divided in 5 folds and, by rotation, we are training using 4 folds (n-1) and validate using the 5th (nth) fold.
+
+kf = KFold(n_splits = NUMBER_KFOLDS, random_state = RANDOM_STATE, shuffle = True)
+for train_index, test_index in kf.split(df):
+    train_X, valid_X = df.iloc[train_index], df.iloc[test_index]
+
+    dtrain = lgb.Dataset(train_X[predictors].values, label=train_X[target].values,
+                     feature_name=predictors)
+
+    dvalid = lgb.Dataset(valid_X[predictors].values, label=valid_X[target].values,
+                     feature_name=predictors)
+
+    evals_results = {}
+    model =  lgb.train(params, 
+                  dtrain, 
+                  valid_sets=[dtrain, dvalid], 
+                  valid_names=['train','valid'], 
+                  evals_result=evals_results, 
+                  num_boost_round=MAX_ROUNDS,
+                  early_stopping_rounds=EARLY_STOP,
+                  verbose_eval=VERBOSE_EVAL, 
+                  feval=None)
+
+
+#The average validation AUC for the 5 folds obtained was 0.9818, with values between 0.975 and 0.989.
+    
+# =============================================================================
+# Conclusions   
+# We investigated the data, checking for data unbalancing, visualizing the features and understanding the relationship between different features. We then presented the data to a LightGBM model. We used both train-validation split and cross-validation to evaluate the model effectiveness to predict 'Class' value, i.e. detecting if a transaction was fraudulent. With both methods obtained values of AUC for the validation set around 0.98.
+# =============================================================================
+
+
+
+
+# Get all the columns from the dataFrame
+columns = df.columns.tolist()
+
+# Filter the columns to remove data we do not want
+columns = [c for c in columns if c not in ["Class"]]
+
+# Store the variable we'll be predicting on
+target = "Class"
+
+X = df[columns]
+Y = df[target]
+
+# Print shapes
+print(X.shape)
+print(Y.shape)
+
+
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+# define random states
+state = 1
+
+# define outlier detection tools to be compared
+classifiers = {
+    "Isolation Forest": IsolationForest(max_samples=len(X),
+                                        contamination=outlier_fraction,
+                                        random_state=state),
+    "Local Outlier Factor": LocalOutlierFactor(
+        n_neighbors=20,
+        contamination=outlier_fraction)}
+    
+# Fit the model
+plt.figure(figsize=(9, 7))
+n_outliers = len(Fraud)
+
+
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    
+    # fit the data and tag outliers
+    if clf_name == "Local Outlier Factor":
+        y_pred = clf.fit_predict(X)
+        scores_pred = clf.negative_outlier_factor_
+    else:
+        clf.fit(X)
+        scores_pred = clf.decision_function(X)
+        y_pred = clf.predict(X)
+    
+    # Reshape the prediction values to 0 for valid, 1 for fraud. 
+    y_pred[y_pred == 1] = 0
+    y_pred[y_pred == -1] = 1
+    
+    n_errors = (y_pred != Y).sum()
+    
+    # Run classification metrics
+    print('{}: {}'.format(clf_name, n_errors))
+    print(accuracy_score(Y, y_pred))
+    print(classification_report(Y, y_pred))
+
